73cbb4344b7a7dc7a59af71caaf3d2de75cccafb
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-04-14 23:21:56 -0400

    recipe-card updated

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  FocusNode _focus = new FocusNode();
+  TextEditingController _searchController;
+  bool _showSearch = false;
   
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: appBar,
+      appBar: searchAppBar(),
       body: recipeBody,
       bottomNavigationBar: bottombar,
     );
@@ -61,26 +63,69 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     ),
   );
-  
-  final appBar = AppBar(
-    backgroundColor: Colors.pink,
-    title: Text("Foodie Bells"),
-    actions: <Widget>[
-      IconButton(
-        icon: Icon(Icons.list),
-        onPressed: () {},
-      )
-    ],
-  );
+
+  Widget searchAppBar(){
+    return new AppBar(
+      backgroundColor: Colors.pink,
+      leading: IconButton(
+        icon: Icon(Icons.settings),
+        //color: Colors.black,
+        onPressed: () {
+         },
+      ),
+      title: GestureDetector(
+        onTap: (){
+          setState(() {
+            _showSearch = true;
+          });
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical:15.0),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(5.0)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal:8.0),
+            child: TextFormField(
+              controller: _searchController,
+              decoration: InputDecoration.collapsed(hintText: "Search"),
+              focusNode: _focus,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   final recipeBody = Container(
-    child: ListView.builder(
+    child: ListView(
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return new RecipeCard();
-      },
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: new Text("Featured Recipe",style: TextStyle(fontSize: 22.0,fontWeight: FontWeight.w500),),
+        ),
+        new RecipeCard(new Size(200.0,200.0),"Potato Samosa"),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: new Text("Categories",style: TextStyle(fontSize: 22.0,fontWeight: FontWeight.w500),),
+        ),
+        Container(
+          height: 150.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            children: <Widget>[
+              new RecipeCard(new Size(100.0,100.0)),
+              new RecipeCard(new Size(100.0,100.0)),
+              new RecipeCard(new Size(100.0,100.0)),
+              new RecipeCard(new Size(100.0,100.0)),
+              new RecipeCard(new Size(100.0,100.0)),
+            ],
+          ),
+        )
+      ],
     ),
   );
 }
+
--- lib/recipe-card.dart
+++ lib/recipe-card.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class RecipeCard extends StatefulWidget{
+  Size size;
+  String title;
+
+  RecipeCard([this.size,this.title]);
 
   @override
   _RecipeCardState createState() => new _RecipeCardState();
@@ -14,9 +18,14 @@ class _RecipeCardState extends State<RecipeCard>{
         elevation: 8.0,
         margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
         child: new Column(
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            new Image.network('https://foodiebells.com/img/recipes/potato-samosas/samosa.jpg'),
-            new Padding(
+            new Image.network('https://foodiebells.com/img/recipes/potato-samosas/samosa.jpg',
+
+              width: widget.title!=null?MediaQuery.of(context).size.width:widget.size.width,
+              fit:widget.title!=null?BoxFit.contain:BoxFit.cover,
+            ),
+          widget.title!=null?new Padding(
                 padding: new EdgeInsets.all(7.0),
                 child: new Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -33,13 +42,13 @@ class _RecipeCardState extends State<RecipeCard>{
                       ),
                       child: new Padding(
                         padding: new EdgeInsets.all(10.0),
-                        child: new Text("Potato Samosa ",style: new TextStyle(color: Colors.white),),
+                        child: new Text(widget.title + " ",style: new TextStyle(color: Colors.white),),
                       ),
                     ),
                     Icon(Icons.arrow_forward_ios)
                   ],
                 )
-            )
+            ):Text('')
           ],
         ),
       );
